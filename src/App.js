902690ef6b8cd3fe9c7fec7dab98fd3d4a135daa
yordigenet <105040859+yordigenet@users.noreply.github.com>
2023-10-12 22:31:56 -0700

    added Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Weather from './weather';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
         <h1>React Weather App</h1>
         <Weather/>
-      </header>
+      </header></div>
     </div>
   );
 }
 
-export default App;
