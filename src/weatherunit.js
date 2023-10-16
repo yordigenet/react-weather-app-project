efd57e161e614cbadb6f3882c64cad229bc6e9b0
yordigenet <105040859+yordigenet@users.noreply.github.com>
2023-10-16 00:26:26 -0700

    updated weatherunit.js to convert to °f and °c

--- src/weatherunit.js
+++ src/weatherunit.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Weatherunit(){
-    return(
+export default function Weatherunit(props){
+    const [unit, setUnit] = useState("celcius")
+
+    function convertToFahranite(event){
+    event.preventDefault();
+    setUnit("fahrenheitTemp")
+}
+
+function convertToCelcius(event){
+    event.preventDefault();
+    setUnit("celcius")
+}
+
+function fahrenheitTemp(){
+    return(Math.round(((props.celcius * 9/5) + 32)));
+}
+
+    if (unit === "celcius"){
+        return(
         <div className="weatherUnit">
-            <span className="temprature">
-        </span><span className="units">°C | °F</span>
+            <span className="temprature">{props.celcius}
+        </span><span className="units">°C | <a href="#section" onClick={convertToFahranite}>°F</a></span>
         </div>
-    )
+    );}
+   else {
+    return (
+        <div className="weatherUnit">
+            <span className="temprature">{fahrenheitTemp()}
+        </span><span className="units"><a href="#section" onClick={convertToCelcius}>°C</a> | °F</span>
+        </div>
+    );
+   } 
 }
