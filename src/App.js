14f69256ec77708363ecde0d749a12fdca05ab3e
yordigenet <105040859+yordigenet@users.noreply.github.com>
2023-10-16 00:52:59 -0700

    added a link to netlify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <div className='weather-container'>
         <Weather defaultCity="Addis Ababa"/></div><footer>
-      <p>This project is coded by <a href="https://www.shecodes.io/graduates/83359-yordanos-girma" target='blank'>Yordanos Girma</a> and open-source on <a href="https://github.com/yordigenet/react-weather-app" target='blank'>Github</a> and hosted on <a href="https://github.com/yordigenet/react-weather-app" target='blank'>Netlify</a></p>
+      <p>This project is coded by <a href="https://www.shecodes.io/graduates/83359-yordanos-girma" target='blank'>Yordanos Girma</a> and open-source on <a href="https://github.com/yordigenet/react-weather-app-project" target='blank'>Github</a> and hosted on <a href="https://remarkable-kleicha-136100.netlify.app/" target='blank'>Netlify</a></p>
       </footer></div>
     </div>
   );
